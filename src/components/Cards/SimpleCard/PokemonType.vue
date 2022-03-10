f661ce09b5909dda41b1cb96db7485577e1866a8
VichithLy <ly.vichith@gmail.com>
2022-03-10 15:13:07 +0100

    create type tooltip

--- src/components/Cards/SimpleCard/PokemonType.vue
+++ src/components/Cards/SimpleCard/PokemonType.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="sc-type-wrapper" :class="'back-color-' + typeColor">
+  <div class="sc-type-wrapper has-tooltip" :class="'back-color-' + typeColor">
     <img class="sc-type-picture" :src="getImg(typeColor)" :alt="typeColor" />
+    <span
+      class="capitalize tooltip rounded-lg shadow-2xl py-1 px-2 bg-gray-100 -mt-20 border border-black"
+      >{{ typeColor }}</span
+    >
   </div>
 </template>
 
